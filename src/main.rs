8290d9683047aba256fb2c01496a67868cb04555
PottierLoic <loic.pottier@live.fr>
2024-04-14 02:42:37 +0200

    feat: create basic window setup

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use winit::{
+  event::{Event, WindowEvent},
+  event_loop::{ControlFlow, EventLoop},
+  window::WindowBuilder,
+};
+
+fn main() {
+  let event_loop = EventLoop::new();
+  let window = WindowBuilder::new().build(&event_loop).unwrap();
+  window.set_title("Attractor");
+  env_logger::init();
+
+  event_loop.run(move |event, _, control_flow| {
+    *control_flow = ControlFlow::Wait;
+
+    match event {
+      Event::WindowEvent {
+        event: WindowEvent::CloseRequested,
+        ..
+      } => *control_flow = ControlFlow::Exit,
+      _ => (),
+    }
+  });
+}
